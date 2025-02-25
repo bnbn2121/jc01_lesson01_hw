285cc7a87ad4fed680c19186041a5b25a78030a6
bnbn2121 <shelutods@gmail.com>
2025-02-25 20:24:51 +0300

    Add files via upload

---
+++ src/edu/training/jc/simple_output/FrameOutput_03.java
@@ -0,0 +1,13 @@
+package edu.training.jc.simple_output;
+
+public class FrameOutput_03 {
+
+	public static void main(String[] args) {
+		System.out.println("***********");
+		System.out.println("*    *    *");
+		System.out.println("*    *    *");
+		System.out.println("*    *    *");
+		System.out.println("***********");
+	}
+
+}
---
+++ src/edu/training/jc/simple_output/TextOutput_05.java
@@ -0,0 +1,14 @@
+package edu.training.jc.simple_output;
+
+public class TextOutput_05 {
+
+	public static void main(String[] args) {
+		System.out.println("\tLong, long ago there lived a "
+				+ "young\nand very beautiful girl called Cinderella.");
+		System.out.println("\tCinderella lived with her "
+				+ "stepmother\nand stepsisters. The sisters were "
+				+ "wicked and\nhaughty.");
+
+	}
+
+}
