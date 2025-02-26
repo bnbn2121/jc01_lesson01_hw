6a1cd68c8468d9fb04289233977edf7fb64c6216
bnbn2121 <shelutods@gmail.com>
2025-02-26 09:11:20 +0300

    Update FormattingWithinString_04.java

--- src/edu/training/jc/simple_output/FormattingWithinString_04.java
+++ src/edu/training/jc/simple_output/FormattingWithinString_04.java
@@ -4,8 +4,8 @@ public class FormattingWithinString_04 {
 
 	public static void main(String[] args) {
 		System.out.println(	"1) Todoist - Balancing power and simplicity.\n"+
-							"2) TickTick - Embedded calendars and timers.\n"+
-							"3) Microsoft To Do - Microsoft power users.");
+					"2) TickTick - Embedded calendars and timers.\n"+
+					"3) Microsoft To Do - Microsoft power users.");
 	}
 
 }
