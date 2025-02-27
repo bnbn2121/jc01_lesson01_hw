898b2b90e8d718d6cc29716d56b11036d3890473
bnbn2121 <shelutods@gmail.com>
2025-02-27 22:29:52 +0300

    Update and rename SimpleConsoleOutput_02.java to SimpleConsoleOutput02.java

--- src/edu/training/jc/simple_output/SimpleConsoleOutput02.java
+++ src/edu/training/jc/simple_output/SimpleConsoleOutput02.java
@@ -1,6 +1,6 @@
 package edu.training.jc.simple_output;
 
-public class SimpleConsoleOutput_02 {
+public class SimpleConsoleOutput02 {
 
 	public static void main(String[] args) {
 		System.out.println("Yesterday all my troubles seemed so "
