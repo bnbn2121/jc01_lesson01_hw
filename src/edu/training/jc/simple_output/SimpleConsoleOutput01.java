268921aa4d28856e3f427a4917ec151b32c42307
bnbn2121 <shelutods@gmail.com>
2025-02-27 22:29:40 +0300

    Update and rename SimpleConsoleOutput_01.java to SimpleConsoleOutput01.java

--- src/edu/training/jc/simple_output/SimpleConsoleOutput01.java
+++ src/edu/training/jc/simple_output/SimpleConsoleOutput01.java
@@ -1,6 +1,6 @@
 package edu.training.jc.simple_output;
 
-public class SimpleConsoleOutput_01 {
+public class SimpleConsoleOutput01 {
 
 	public static void main(String[] args) {
 		System.out.println("A\nB\nC\nD\nE");
