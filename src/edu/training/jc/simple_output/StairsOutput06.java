7258c264e0d9e7b47b68235c7aa81345b2964d63
bnbn2121 <shelutods@gmail.com>
2025-02-27 22:30:03 +0300

    Update and rename StairsOutput_06.java to StairsOutput06.java

--- src/edu/training/jc/simple_output/StairsOutput06.java
+++ src/edu/training/jc/simple_output/StairsOutput06.java
@@ -1,6 +1,6 @@
 package edu.training.jc.simple_output;
 
-public class StairsOutput_06 {
+public class StairsOutput06 {
 
 	public static void main(String[] args) {
 		System.out.println("\"To wisely live your life, you don't need to know much\n\n"
