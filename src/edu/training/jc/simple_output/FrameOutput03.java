5d7036c174c74c82d682d33f387f2755f2f52163
bnbn2121 <shelutods@gmail.com>
2025-02-27 22:29:27 +0300

    Update and rename FrameOutput_03.java to FrameOutput03.java

--- src/edu/training/jc/simple_output/FrameOutput03.java
+++ src/edu/training/jc/simple_output/FrameOutput03.java
@@ -1,6 +1,6 @@
 package edu.training.jc.simple_output;
 
-public class FrameOutput_03 {
+public class FrameOutput03 {
 
 	public static void main(String[] args) {
 		System.out.println("***********");
