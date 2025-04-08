f230636d60b965aef0c459f562a0808fad13dc1f
Dmitry Sheluto <shelutods@gmail.com>
2025-04-08 18:00:43 +0300

    эксперементирую с гитом

--- src/edu/training/jc/simple_output/SimpleConsoleOutput01.java
+++ src/edu/training/jc/simple_output/SimpleConsoleOutput01.java
@@ -3,7 +3,8 @@ package edu.training.jc.simple_output;
 public class SimpleConsoleOutput01 {
 
 	public static void main(String[] args) {
-		System.out.println("A\nB\nC\nD\nE");
+		System.out.println("A\nB\nC\nD");
+		System.out.println("E")
 	}
 
 }
